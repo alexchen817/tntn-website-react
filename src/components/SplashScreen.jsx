0dc829e42c941fb88c9f6fa12421eaea5583e355
Alexander Chen <alexanderchen817@gmail.com>
2025-12-30 12:46:42 -0800

    add simple splash screen animation logo

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -1,11 +1,33 @@
-
-
+import { useRef } from "react"
+import { useGSAP } from "@gsap/react"
+import gsap from "gsap"
 const SplashScreen = () => {
-  return (
-    <div className='h-screen flex bg-gray-950 justify-center place-items-center'>
-        <h1 className='text-9xl font-bold text-gray-100'>Welcome.</h1>
-    </div>
-  )
+    const container = useRef();
+    // animate the logo 
+    useGSAP(() => {
+        const timeline = gsap.timeline();
+        // initially hide the logo
+        gsap.set(".logo", { opacity: 0 });
+        // 1) hide 2) appear 3) fade
+        timeline.to(".logo", {
+            opacity: 100,
+            scale: 10,
+            duration: 1.5,
+            ease: "power4.inOut"
+        })
+            .to(".logo", {
+                opacity: 0,
+                duration: 1.5,
+                ease: "power1.inOut"
+            });
+
+    }, [])
+
+    return (
+        <div className='h-screen flex bg-black justify-center place-items-center'>
+            <img src="/tntnlogo.png" alt="TNTN Logo" className="logo w-10" />
+        </div>
+    )
 }
 
 export default SplashScreen
