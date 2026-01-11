617eaf2460d3520ac49ff487843bb2120bf73331
Alexander Chen <alexanderchen817@gmail.com>
2026-01-10 22:31:28 -0800

    change navbar bg to lighter gradient

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,23 +31,14 @@ const Navbar = () => {
         })
     }, []);
 
-    // function to scroll to specific section
-    const handleScroll = (e, target) => {
-        gsap.to(window, {
-            duration: 1.2,
-            scrollTo: { y: target, autoKill: false },
-            ease: "power3.inOut"
-        });
-    };
-
     return (
-        <nav ref={navRef} className="fixed z-50 top-0 left-0 w-full bg-black px-6 py-4">
+        <nav ref={navRef} className="fixed z-50 top-0 left-0 w-full bg-dark-bg px-6 py-4">
             <div className="max-w-7xl mx-auto flex justify-between items-center">
                 <div className="text-white font-bold text-xl">
                     <img src={tntn} alt="TNTN text logo" className='w-50' />
                 </div>
                 <div className="flex gap-8 text-sm font-medium text-zinc-400">
-                    <a href="#about" onClick={(e) => handleScroll(e, "#about")} className="hover:text-white transition-colors">About</a>
+                    <a href="#about" className="hover:text-white transition-colors">About</a>
                     <a href="#team" className="hover:text-white transition-colors">Team</a>
                     <a href="#media" className="hover:text-white transition-colors">Media</a>
                     <a href="#events" className="hover:text-white transition-colors">Events</a>
